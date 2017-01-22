fc8b2fc746f10d3f5e5f2237d2d3273949d1d550
Octogonapus <firey45@gmail.com>
2017-01-22 01:38:13 -0500

    working on skills

--- motorControl.c
+++ motorControl.c
@@ -155,12 +155,15 @@ void scoreFence(const fenceTypes fence)
 	switch (fence)
 	{
 		case FENCE_LEFT:
-			moveToPoint(3543, 1118); //Center of left segment
-			turnToAbsAngle(180);
-			//knock the stars off
 			break;
 
 		case FENCE_MIDDLE:
+			break;
+
+	  case FENCE_RIGHT:
+			moveToPoint(3100, 1118); //Center of right segment
+			turnToAbsAngle(180);
+			//knock the stars off
 			break;
 
 		default:
@@ -238,6 +241,110 @@ void pickUpCube(const long x, const long y)
 
 	intakeAndLiftTask_intakeState = INTAKE_OPEN;
 	intakeAndLiftTask_liftState = LIFT_DOWN;
+}
+
+/**
+ * Pick up stars against the wall during skills
+ */
+void cheeseThoseStars()
+{
+	vel_PID intakePID, drivePID;
+
+	//Save left and right quad values instead of setting them to zero
+	const long encoderLeft = SensorValue[leftQuad], encoderRight = SensorValue[rightQuad];
+
+	//Total distance elapsed since start and total angle change since start
+	float distanceElapsed = 0, angleChange = 0;
+	float lastDistance = 0;
+
+  //Conversion between encoder degrees and base_link mm
+  const float conv = 1.311250;
+
+	//Target distance for the distance PID controller
+	//Angle PID controller's target is 0
+	int targetDistance = -610 * conv;
+
+	pos_PID distancePID, anglePID;
+
+	pos_PID_InitController(&distancePID, &distanceElapsed, 0.2, 0.2, 0.1);
+	pos_PID_InitController(&anglePID, &angleChange, 0.5, 0.25, 0);
+
+	pos_PID_SetTargetPosition(&distancePID, targetDistance);
+	pos_PID_SetTargetPosition(&anglePID, 0);
+
+	//If distance PID controller is at target
+	bool atTarget = false;
+
+	//Distance that is "close enough" to target
+	const int atTargetDistance = 15;
+
+	//Threshold for not moving
+	const int threshold = 2;
+
+	//Timer for being at target
+	timer atTargetTimer;
+	timer_Initialize(&atTargetTimer);
+
+	//Timeout period (ms)
+	const int timeoutPeriod = 250;
+
+	//Current left and right quad displacements
+	long currentLeft, currentRight;
+
+	//Distance and angle PID output
+	int distOutput, angleOutput;
+
+	while (!atTarget)
+	{
+		//Calculate distance displacement
+		currentLeft = SensorValue[leftQuad] - encoderLeft;
+		currentRight = SensorValue[rightQuad] - encoderRight;
+
+		//Overall displacement is the average of left and right displacements
+		distanceElapsed = (currentLeft + currentRight) / 2.0;
+
+		//Angle change doesn't need to be a real angle, just the difference in
+		//displacements
+		angleChange = currentLeft - currentRight;
+
+		//Get output from both PID's
+		distOutput = pos_PID_StepController(&distancePID);
+		angleOutput = pos_PID_StepController(&anglePID);
+
+		//Set motors to distance PID output with correction from angle PID
+		setLeftMotors(distOutput + angleOutput);
+		setRightMotors(distOutput - angleOutput);
+
+		writeDebugStreamLine("%d",pos_PID_GetError(&distancePID));
+
+		//Place mark if we're close enough to the target distance
+		if (fabs(targetDistance - distanceElapsed) <= atTargetDistance)
+		{
+			timer_PlaceHardMarker(&atTargetTimer);
+		}
+		//Place mark if we haven't moved much
+		else if (fabs(distanceElapsed - lastDistance) <= threshold)
+		{
+			timer_PlaceHardMarker(&atTargetTimer);
+		}
+		else
+		{
+			timer_ClearHardMarker(&atTargetTimer);
+		}
+
+		lastDistance = distanceElapsed;
+
+		//If we've been close enough for long enough, we're there
+		if (timer_GetDTFromHardMarker(&atTargetTimer) >= timeoutPeriod)
+		{
+			atTarget = true;
+		}
+
+		wait1Msec(15);
+	}
+
+	setAllDriveMotors(0);
+	writeDebugStreamLine("target: %1.2f", targetDistance);
 }
 
 #endif //MOTORCONTROL_C_INCLUDED
--- skills.c
+++ skills.c
@@ -4,10 +4,24 @@
 void runSkills()
 {
   //Close claw and drive back
-  intakeAndLiftTask_intakeState = INTAKE_REST;
-  intakeAndLiftTask_liftState = LIFT_DOWN;
+  intakeAndLiftTask_intakeState = INTAKE_REST; //Let the intake chill
+  intakeAndLiftTask_liftState = LIFT_DOWN; //Put the lift down
   startTask(intakeAndLiftTask);
-  driveStraight(-1118);
+
+  intakeAndLiftTask_intakeState = INTAKE_OPEN;
+  cheeseThoseStars();
+
+  // driveStraight(-610); //Drive back off the tile
+  // intakeAndLiftTask_intakeState = INTAKE_CLOSED; //Close the intake
+  //
+  // //Wait until intake is closed
+  // while (intakeAndLiftTask_intakeStateRead != INTAKE_CLOSED) { wait1Msec(5); }
+  //
+  // dumpIntake(); //Score what we picked up from the starting tile
+  //
+  // scoreFence(FENCE_RIGHT); //Knock the stars off the rightmost fence segment
+  //
+  // moveToPoint();
 }
 
 #endif //SKILLS_C_INCLUDED

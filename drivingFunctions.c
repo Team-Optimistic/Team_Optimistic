cfa9c6b301bd7d700b90092d16f1ddabe684a11f
Octogonapus <firey45@gmail.com>
2017-03-19 22:20:51 -0400

    Added ballsy functions

--- drivingFunctions.c
+++ drivingFunctions.c
@@ -111,4 +111,114 @@ void driveStraight(const long distance)
 	//writeDebugStreamLine("target: %1.2f", targetDistance);
 }
 
+/**
+ * Drives in a straight line for a distance in mm
+ * @param  distance Distance to drive for (mm)
+ */
+void driveStraight_Ballsy(const long distance)
+{
+	//Save left and right quad values instead of setting them to zero
+	const long encoderLeft = SensorValue[leftQuad], encoderRight = SensorValue[rightQuad];
+
+	//Total distance elapsed since start and total angle change since start
+	float distanceElapsed = 0, angleChange = 0;
+	float lastDistance = 0;
+
+  //Conversion between encoder degrees and base_link mm
+  const float conv = 1.345;
+
+	//Target distance for the distance PID controller
+	//Angle PID controller's target is 0
+	int targetDistance = distance * conv;
+
+	pos_PID distancePID, anglePID;
+
+	if (distance <= 800)
+	{
+		//pos_PID_InitController(&distancePID, &distanceElapsed, 0.2, 0.2, 0.1);
+		pos_PID_InitController(&distancePID, &distanceElapsed, 0.4, 0.25, 0);
+		pos_PID_InitController(&anglePID, &angleChange, 0.5, 0.25, 0);
+	}
+	else
+	{
+		pos_PID_InitController(&distancePID, &distanceElapsed, 0.3, 0.2, 0);
+		pos_PID_InitController(&anglePID, &angleChange, 0.5, 0.7, 0);
+	}
+
+	pos_PID_SetTargetPosition(&distancePID, targetDistance);
+	pos_PID_SetTargetPosition(&anglePID, 0);
+
+	//If distance PID controller is at target
+	bool atTarget = false;
+
+	//Distance that is "close enough" to target
+	const int atTargetDistance = 15;
+
+	//Threshold for not moving
+	const int threshold = 2;
+
+	//Timer for being at target
+	timer atTargetTimer;
+	timer_Initialize(&atTargetTimer);
+
+	//Timeout period (ms)
+	const int timeoutPeriod = 250;
+
+	//Current left and right quad displacements
+	long currentLeft, currentRight;
+
+	//Distance and angle PID output
+	int distOutput, angleOutput;
+
+	while (!atTarget)
+	{
+		//Calculate distance displacement
+		currentLeft = SensorValue[leftQuad] - encoderLeft;
+		currentRight = SensorValue[rightQuad] - encoderRight;
+
+		//Overall displacement is the average of left and right displacements
+		distanceElapsed = (currentLeft + currentRight) / 2.0;
+
+		//Angle change doesn't need to be a real angle, just the difference in
+		//displacements
+		angleChange = currentLeft - currentRight;
+
+		//Get output from both PID's
+		distOutput = pos_PID_StepController(&distancePID);
+		angleOutput = pos_PID_StepController(&anglePID);
+
+		//Set motors to distance PID output with correction from angle PID
+		setLeftMotors(distOutput + angleOutput);
+		setRightMotors(distOutput - angleOutput);
+
+		//Place mark if we're close enough to the target distance
+		if (fabs(targetDistance - distanceElapsed) <= atTargetDistance)
+		{
+			break;
+		}
+		//Place mark if we haven't moved much
+		else if (fabs(distanceElapsed - lastDistance) <= threshold)
+		{
+			timer_PlaceHardMarker(&atTargetTimer);
+		}
+		else
+		{
+			timer_ClearHardMarker(&atTargetTimer);
+		}
+
+		lastDistance = distanceElapsed;
+
+		//If we've been close enough for long enough, we're there
+		if (timer_GetDTFromHardMarker(&atTargetTimer) >= timeoutPeriod)
+		{
+			break;
+		}
+
+		wait1Msec(15);
+	}
+
+	setAllDriveMotors(0);
+	//writeDebugStreamLine("target: %1.2f", targetDistance);
+}
+
 #endif //DRIVINGFUNCTIONS_C_INCLUDED
--- turningFunctions.c
+++ turningFunctions.c
@@ -102,6 +102,109 @@ void turn(long angle)
 }
 
 /**
+ * Turns clockwise for an angle in degrees
+ * @param  angle Angle to turn for (deg)
+ */
+void turn_Ballsy(long angle)
+{
+	//Save left and right quad values instead of setting them to zero
+	long encoderLeft = SensorValue[leftQuad], encoderRight = SensorValue[rightQuad];
+
+	//Total angle change since start
+	float angleChange = 0, lastAngle = 0;
+
+	//Conversion between encoder degrees and base_link degrees
+	const float conv = 12.75993;
+
+	//Fix angle
+	while(angle>180)
+      angle-=360;
+  while(angle<=-180)
+      angle+=360;
+
+	int targetAngle = angle * conv;
+
+	pos_PID anglePID;
+	if (fabs(angle) <= 350)
+	{
+		pos_PID_InitController(&anglePID, &angleChange, 0.6, 0.45, 0.1 * (angle / 180.0));
+	}
+	else
+	{
+		pos_PID_InitController(&anglePID, &angleChange, 0.6, 0.45, 0.2 * (angle / 180.0));
+	}
+	pos_PID_SetTargetPosition(&anglePID, targetAngle);
+
+	//If angle PID controller is at target
+	bool atTarget = false;
+
+	//Angle that is "close enough" to target
+	const int atTargetAngle = 10;
+
+	//Threshold for not moving
+	const int threshold = 2;
+
+	//Timer for being at target
+	timer atTargetTimer, exitTimer;
+	timer_Initialize(&atTargetTimer);
+	timer_Initialize(&exitTimer);
+
+	//Timeout period (ms)
+	const int timeoutPeriod = 250;
+
+	//Current left and right quad displacements
+	long currentLeft, currentRight;
+
+	//Distance and angle PID output
+	int angleOutput;
+
+	while (!atTarget)
+	{
+		//Calculate distance displacement
+		currentLeft = SensorValue[leftQuad] - encoderLeft;
+		currentRight = SensorValue[rightQuad] - encoderRight;
+
+		angleChange = currentRight - currentLeft;
+
+		//Get output from PID
+		angleOutput = pos_PID_StepController(&anglePID);
+
+		//Set motors to angle PID output
+		setLeftMotors(-1 * angleOutput);
+		setRightMotors(angleOutput);
+
+		//Place mark if we're close enough to the target angle
+		if (fabs(targetAngle - angleChange) <= atTargetAngle)
+		{
+			timer_PlaceHardMarker(&atTargetTimer);
+			timer_PlaceHardMarker(&exitTimer);
+		}
+		//Place mark if we haven't moved much
+		else if (fabs(angleChange - lastAngle) <= threshold)
+		{
+			timer_PlaceHardMarker(&atTargetTimer);
+		}
+		else
+		{
+			timer_ClearHardMarker(&atTargetTimer);
+		}
+
+		lastAngle = angleChange;
+
+		//If we've been close enough for long enough, we're there
+		if (timer_GetDTFromHardMarker(&atTargetTimer) >= timeoutPeriod ||
+				timer_GetDTFromHardMarker(&exitTimer) >= 50)
+		{
+			break;
+		}
+
+		wait1Msec(15);
+	}
+
+	setAllDriveMotors(0);
+}
+
+/**
  * Turns to an angle in the field frame
  * @param deg Degrees to turn to
  */
